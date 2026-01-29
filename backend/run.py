9798ada5830e4f7a1a3b620e804ea6f71f3467a6
Yasith Heshan <yheshan1@gmail.com>
2026-01-29 19:31:53 +0530

    fix: update port configuration for FastAPI to use 8001 across Dockerfile, makefile, run.py, and docker-compose.yml

--- backend/run.py
+++ backend/run.py
@@ -6,7 +6,7 @@ APP_ENV = os.getenv("APP_ENV", "development")
 if __name__ == "__main__":
     if APP_ENV == "development":
         print("Running in development mode.")
-        uvicorn.run("app.main:app", host="0.0.0.0", port=8080, reload=True)
+        uvicorn.run("app.main:app", host="0.0.0.0", port=8001, reload=True)
     else:
         print("Running in production mode.")
-        uvicorn.run("app.main:app", host="0.0.0.0", port=8080, reload=False)
+        uvicorn.run("app.main:app", host="0.0.0.0", port=8001, reload=False)
